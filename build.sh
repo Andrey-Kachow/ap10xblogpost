cde9ae8de2e1e3e09a1b44abaab237e66604daf5
Andrey Popov <andr10xp@gmail.com>
2022-12-29 18:26:59 +0100

    cleanup build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,5 @@ export AP10X_RES_PATH=$dir"/src/main/resources/"
 echo $AP10X_RES_PATH
 
 mvn clean
-#mvn compile
-#mvn dependency:copy-dependencies
 mvn package
-#jar uf target/andreypopovblogpost.jar META-INF
 java -jar target/andreypopovblogpost-jar-with-dependencies.jar
