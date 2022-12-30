74a209d11ff1f6f25d075a02236a08498cc29784
Andrey Popov <andr10xp@gmail.com>
2022-12-30 06:00:30 +0100

    made html css of the page more nice. Set viewport

--- src/main/java/ap10x/Settings.java
+++ src/main/java/ap10x/Settings.java
@@ -2,6 +2,5 @@ package ap10x;
 
 public class Settings {
   public static final boolean DEBUG = true;
-
   public static final String IMAGES_ROOT = System.getenv("AP10X_IMG_PATH");
 }
--- src/main/java/ap10x/view/BasePage.java
+++ src/main/java/ap10x/view/BasePage.java
@@ -32,6 +32,7 @@ public class BasePage implements RenderComponent {
       <html lang="en">
       <head>
           <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>""");
     out.print(title);
     out.print("</title>\n");
